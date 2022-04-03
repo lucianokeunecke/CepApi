fa696c6fded855820cbd305bb2f10fb02f2706d1
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-04-03 11:11:01 -0300

    Trabalho da disciplina Integração Continua e DevPps

--- src/main/java/br/com/infinet/cep/controller/CepController.java
+++ src/main/java/br/com/infinet/cep/controller/CepController.java
@@ -1,22 +1,35 @@
 package br.com.infinet.cep.controller;
 
 import br.com.infinet.cep.model.CepModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CepController implements Serializable {
 
     private static final long serialVersionUID = 6046704732666502085L;
 
-    @GetMapping(value="/getCep/{cep}")
+    @GetMapping(value="/cep/{cep}")
     public ResponseEntity<CepModel> doObterCep(@PathVariable(name = "cep") String cep) {
 
+        RestTemplate restTemplate = new RestTemplate();
 
-        return ResponseEntity.notFound().build();
+        String uri = "http://viacep.com.br/ws/{cep}/json/";
+
+        Map<String, String> params = new HashMap<String, String>();
+
+        params.put("cep", cep);
+
+        CepModel cepModel = restTemplate.getForObject(uri, CepModel.class, params);
+
+        return new ResponseEntity<CepModel>(cepModel, HttpStatus.OK);
     }
 }
